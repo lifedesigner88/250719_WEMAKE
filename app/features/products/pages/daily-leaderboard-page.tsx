b4ac32bb522afe574936c4034eb257e0649777c1
SejongPark <lifedesigner88@gmail.com>
2025-07-29 17:47:41 +0900

    에러 바운더리 설정중

--- app/features/products/pages/daily-leaderboard-page.tsx
+++ app/features/products/pages/daily-leaderboard-page.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/daily-leaderboard-page";
 import { DateTime } from "luxon";
+import { data, isRouteErrorResponse } from "react-router";
 
 
 export const loader = ({ params }: Route.LoaderArgs) => {
@@ -13,6 +14,24 @@ export const loader = ({ params }: Route.LoaderArgs) => {
     }).setZone("Asia/Seoul");
 
     if (!date.isValid)
-        throw new Error("Invalid date");
+        throw data({
+                error_code:"invalid_date",
+                message:"invalid date"
+            },
+            {
+                status:400
+            }
+        )
+
+}
+
+export default function DailyLeaderboardPage() {
+    return <h1>DailyLeaderboardPage</h1>;
+}
+
+export const ErrorBoundary = ({ error }: Route.ErrorBoundaryProps) => {
+        if(isRouteErrorResponse(error))
+    return <h1>ErrorBoundary</h1>;
+
 
 }
