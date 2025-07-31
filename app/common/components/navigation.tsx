9fdc4a71f2d6928efc73807b22213a55fef17144
SejongPark <lifedesigner88@gmail.com>
2025-07-31 09:20:52 +0900

    User => Users 로 변경

--- app/common/components/navigation.tsx
+++ app/common/components/navigation.tsx
@@ -23,8 +23,8 @@ import {
     SettingsIcon, BellIcon, MessageCircleIcon
 } from "lucide-react";
 
-export default function Navigation({ isLoggedIn, hasNotification, hasMessages }:{
-    isLoggedIn:boolean, hasNotification:boolean, hasMessages:boolean
+export default function Navigation({ isLoggedIn, hasNotification, hasMessages }: {
+    isLoggedIn: boolean, hasNotification: boolean, hasMessages: boolean
 }) {
     return <>
         <nav
@@ -164,110 +164,110 @@ export default function Navigation({ isLoggedIn, hasNotification, hasMessages }:
 
 const menus = [
     {
-        name: "Products",
-        to: "/products",
-        items: [
+        name:"Products",
+        to:"/products",
+        items:[
             {
-                name: "Leaderboards",
-                description: "See the top performers in your community",
-                to: "/products/leaderboards",
+                name:"Leaderboards",
+                description:"See the top performers in your community",
+                to:"/products/leaderboards",
             },
             {
-                name: "Categories",
-                description: "See the top categories in your community",
-                to: "/products/categories",
+                name:"Categories",
+                description:"See the top categories in your community",
+                to:"/products/categories",
             },
             {
-                name: "Search",
-                description: "Search for a product",
-                to: "/products/search",
+                name:"Search",
+                description:"Search for a product",
+                to:"/products/search",
             },
             {
-                name: "Submit a Product",
-                description: "Submit a product to our community",
-                to: "/products/submit",
+                name:"Submit a Product",
+                description:"Submit a product to our community",
+                to:"/products/submit",
             },
             {
-                name: "Promote",
-                description: "Promote a product to our community",
-                to: "/products/promote",
-            },
-        ],
-    },
-    {
-        name: "Jobs",
-        to: "/jobs",
-        items: [
-            {
-                name: "Remote Jobs",
-                description: "Find a remote job in our community",
-                to: "/jobs?location=remote",
-            },
-            {
-                name: "Full-Time Jobs",
-                description: "Find a full-time job in our community",
-                to: "/jobs?type=full-time",
-            },
-            {
-                name: "Freelance Jobs",
-                description: "Find a freelance job in our community",
-                to: "/jobs?type=freelance",
-            },
-            {
-                name: "Internships",
-                description: "Find an internship in our community",
-                to: "/jobs?type=internship",
-            },
-            {
-                name: "Submit a Job",
-                description: "Submit a job to our community",
-                to: "/jobs/submit",
+                name:"Promote",
+                description:"Promote a product to our community",
+                to:"/products/promote",
             },
         ],
     },
     {
-        name: "Community",
-        to: "/community",
-        items: [
+        name:"Jobs",
+        to:"/jobs",
+        items:[
             {
-                name: "All Posts",
-                description: "See all posts in our community",
-                to: "/community",
+                name:"Remote Jobs",
+                description:"Find a remote job in our community",
+                to:"/jobs?location=remote",
             },
             {
-                name: "Top Posts",
-                description: "See the top posts in our community",
-                to: "/community?sort=top",
+                name:"Full-Time Jobs",
+                description:"Find a full-time job in our community",
+                to:"/jobs?type=full-time",
             },
             {
-                name: "New Posts",
-                description: "See the new posts in our community",
-                to: "/community?sort=new",
+                name:"Freelance Jobs",
+                description:"Find a freelance job in our community",
+                to:"/jobs?type=freelance",
             },
             {
-                name: "Create a Post",
-                description: "Create a post in our community",
-                to: "/community/create",
+                name:"Internships",
+                description:"Find an internship in our community",
+                to:"/jobs?type=internship",
+            },
+            {
+                name:"Submit a Job",
+                description:"Submit a job to our community",
+                to:"/jobs/submit",
             },
         ],
     },
     {
-        name: "IdeasGPT",
-        to: "/ideas",
-    },
-    {
-        name: "Teams",
-        to: "/teams",
-        items: [
+        name:"Community",
+        to:"/community",
+        items:[
             {
-                name: "All Teams",
-                description: "See all teams in our community",
-                to: "/teams",
+                name:"All Posts",
+                description:"See all posts in our community",
+                to:"/community",
             },
             {
-                name: "Create a Team",
-                description: "Create a team in our community",
-                to: "/teams/create",
+                name:"Top Posts",
+                description:"See the top posts in our community",
+                to:"/community?sort=top",
+            },
+            {
+                name:"New Posts",
+                description:"See the new posts in our community",
+                to:"/community?sort=new",
+            },
+            {
+                name:"Create a Post",
+                description:"Create a post in our community",
+                to:"/community/create",
+            },
+        ],
+    },
+    {
+        name:"IdeasGPT",
+        to:"/ideas",
+    },
+    {
+        name:"Teams",
+        to:"/teams",
+        items:[
+            {
+                name:"All Teams",
+                description:"See all teams in our community",
+                to:"/teams",
+            },
+            {
+                name:"Create a Team",
+                description:"Create a team in our community",
+                to:"/teams/create",
             },
         ],
     },
