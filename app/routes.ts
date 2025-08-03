c6d50c9bf45e789905e667812cf124be7958d85a
SejongPark <lifedesigner88@gmail.com>
2025-08-03 11:11:54 +0900

    auth

--- app/routes.ts
+++ app/routes.ts
@@ -41,4 +41,23 @@ export default [
         route("/submit", "features/jobs/pages/submit-job-page.tsx"),
     ]),
 
+    ...prefix("auth", [layout("features/auth/layouts/auth-layout.tsx", [
+        route("/login", "features/auth/pages/login-page.tsx"),
+
+        route("/join", "features/auth/pages/join-page.tsx"),
+
+        ...prefix("/otp", [
+            route("/start", "features/auth/pages/otp-start-page.tsx"),
+            route("/complete", "features/auth/pages/otp-complete-page.tsx"),
+        ]),
+
+        ...prefix("/social/:provider", [
+            route("/social", "features/auth/pages/social-start-page.tsx"),
+            route("/complete", "features/auth/pages/social-complete-page.tsx"),
+        ])
+
+
+    ]),
+    ]),
+
 ] satisfies RouteConfig;
