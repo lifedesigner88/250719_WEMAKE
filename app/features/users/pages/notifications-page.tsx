cf1fa27578c235755112fba38a4bd3892326d369
SejongPark <lifedesigner88@gmail.com>
2025-08-03 15:38:44 +0900

    3.41 notifications-page.tsx

--- app/features/users/pages/notifications-page.tsx
+++ app/features/users/pages/notifications-page.tsx
@@ -1,24 +1,24 @@
+import { NotificationCard } from "../components/navigation";
 import type { Route } from "./+types/notifications-page";
-import PageHeader from "~/common/components/page-header";
 
-export const meta: Route.MetaFunction = () => [
-    { title: "Notifications | wemake" },
-];
 
-export function loader({ request }: Route.LoaderArgs) {
-    return {};
-}
-
-export function action({ request }: Route.ActionArgs) {
-    return {};
-}
+export const meta: Route.MetaFunction = () => {
+    return [{ title:"Notifications | wemake" }];
+};
 
 export default function NotificationsPage() {
     return (
         <div className="space-y-20">
-            <PageHeader title="Notifications" />
-            <div className="space-y-6">
-                <p>View and manage your notifications, alerts, and updates.</p>
+            <h1 className="text-4xl font-bold">Notifications</h1>
+            <div className="flex flex-col items-start gap-5">
+                <NotificationCard
+                    avatarUrl="https://github.com/serranoarevalo.png"
+                    avatarFallback="S"
+                    userName="Steve Jobs"
+                    message=" followed you."
+                    timestamp="2 days ago"
+                    seen={false}
+                />
             </div>
         </div>
     );
