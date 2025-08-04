d33f1929e73847d7a5651617abe6cb5f91f5dab7
SejongPark <lifedesigner88@gmail.com>
2025-08-04 11:57:57 +0900

    3.49 Magic UI

--- app/features/auth/layouts/auth-layout.tsx
+++ app/features/auth/layouts/auth-layout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router";
+import { FlickeringGrid } from "~/common/components/ui/flickering-grid";
 
 export default function AuthLayout() {
     return (
         <div className="grid grid-cols-1 lg:grid-cols-2 h-screen">
-            <div className="bg-gradient-to-br from-primary hidden lg:block via-black to-primary/50" />
+            <FlickeringGrid className="hidden lg:block" squareSize={10} color="rgb(255, 0, 0)" maxOpacity={1}/>
             <Outlet />
         </div>
     );
