639c970e9c0891c5a76d8aab9613e87a5770f647
SejongPark <lifedesigner88@gmail.com>
2025-08-14 17:33:58 +0900

    8.7 Dashboard Chart - progressing

---
+++ app/sql/function/get_dashboard_stats.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION get_dashboard_stats(user_id uuid)
+    RETURNS TABLE
+            (
+                views bigint,
+                month text
+            )
+AS
+$$
+BEGIN
+    RETURN QUERY
+        SELECT COUNT(*)                              AS views,
+               to_char(events.created_at, 'YYYY-MM') AS month
+        FROM public.events
+                 JOIN public.profiles ON profiles.profile_id = user_id
+        WHERE event_data ->> 'username' = profiles.username
+        GROUP BY month;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT COUNT(*)                              AS views,
+       to_char(events.created_at, 'YYYY-MM') AS month
+FROM public.events
+         JOIN PUBLIC.profiles ON profiles.profile_id = '5f06c46b-421e-4c42-8064-6da0c1dd117f'
+WHERE event_data ->> 'username' = profiles.username
+GROUP BY month;
+
